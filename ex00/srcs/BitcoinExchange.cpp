c4f23bb710ca80acc8736b27aeea0562292e279f
Mithras Kuipers <mithraskuipers@gmail.com>
2023-09-10 11:54:06 +0200

    Added date checking functionality, including consideration for leap years

--- ex00/srcs/BitcoinExchange.cpp
+++ ex00/srcs/BitcoinExchange.cpp
@@ -47,32 +47,48 @@ BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& other)
 	return (*this);
 }
 
+int isLeapYear(int year)
+{
+	// Check 1: Check if the year is a potential candidate for being a leap year
+	if (year % 4 == 0)
+	{
+		// Check 2: Check if the year is divisible by 100
+		// Years that are divisible by 100 are not leap years unless they are also divisible by 400
+		if (year % 100 != 0)
+		{
+			return (1); // It's a leap year
+		} else
+		{
+			// Check 3: Check if the year is divisible by 400
+			if (year % 400 == 0)
+			{
+				return (1); // It's a leap year
+			} else
+			{
+				return (0); // It's not a leap year
+			}
+		}
+	}
+	else
+	{
+		return (0); // It's not a leap year
+	}
+}
 
 int BitcoinExchange::isDateValid(const std::string& date)
 {
-	// split string on '-'
-	// if there are no 3 parts, throw error
-	// if there are 3 parts, treat them as year/month/day in variables
-	// now various date checks, 
-	// like year cant be lower than 0, 
-	// month lower than 1, month not higher than 12, 
-	// day not smaller than 1, not greater than 31
-
-
 	// Split the input string on '-'
 	std::vector<std::string> parts;
 	std::istringstream iss(date);
 	std::string part;
 	
-	while (std::getline(iss, part, '-'))
-	{
+	while (std::getline(iss, part, '-')) {
 		parts.push_back(part);
 	}
 
 	// Check if there are exactly 3 parts (year, month, day)
-	if (parts.size() != 3)
-	{
-		return false; // If not, the date format is invalid
+	if (parts.size() != 3) {
+		return (0); // If not, the date format is invalid
 	}
 
 	// Convert the parts to integers using atoi
@@ -84,7 +100,7 @@ int BitcoinExchange::isDateValid(const std::string& date)
 	// Now, perform date validation checks
 	if (year < 0 || month < 1 || month > 12 || day < 1 || day > 31)
 	{
-		return false; // Date components are out of valid ranges
+		return (0); // Date components are out of valid ranges
 	}
 
 	// Check for months with 30 days
@@ -92,22 +108,21 @@ int BitcoinExchange::isDateValid(const std::string& date)
 	{
 		if (day > 30)
 		{
-			return false; // Invalid day for this month
+			return (0); // Invalid day for this month
 		}
 	}
-	// Check for February (special rules for leap years)
 	else if (month == 2)
 	{
-		bool isLeapYear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
-		
-		if (isLeapYear && day > 29)
+		// Check for February (special rules for leap years)
+		if ((!isLeapYear(year)) && (day > 28))
 		{
-			return false; // February in a leap year can have at most 29 days
+			return (0); // February in a non-leap year can have at most 28 days
 		}
-		else if (!isLeapYear && day > 28)
+		else if ((isLeapYear(year)) && (day > 29))
 		{
-			return false; // February in a non-leap year can have at most 28 days
+			return (0); // February in a leap year can have at most 29 days
 		}
 	}
-	return true; // If all conditions are met, the date is valid
+
+	return (1); // If all conditions are met, the date is valid
 }
