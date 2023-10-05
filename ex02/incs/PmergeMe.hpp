0e85973e3be5fe961bfda3e5a99a780794ebc206
Mithras Kuipers <mithraskuipers@gmail.com>
2023-10-05 21:40:11 +0200

    update

--- ex02/incs/PmergeMe.hpp
+++ ex02/incs/PmergeMe.hpp
@@ -6,13 +6,16 @@
 /*   By: mikuiper <mikuiper@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/02 12:31:43 by mikuiper      #+#    #+#                 */
-/*   Updated: 2023/10/04 14:03:33 by mikuiper      ########   odam.nl         */
+/*   Updated: 2023/10/05 21:38:56 by mikuiper      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PMERGE_ME_HPP
 #define PMERGE_ME_HPP
 
-
+#include <iostream>
+#include <sstream>
+#include <cstdlib>
+#include <cctype>
 
 #endif
--- ex02/srcs/main.cpp
+++ ex02/srcs/main.cpp
@@ -6,12 +6,50 @@
 /*   By: mikuiper <mikuiper@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/09/16 17:09:26 by mikuiper      #+#    #+#                 */
-/*   Updated: 2023/10/04 13:59:28 by mikuiper      ########   odam.nl         */
+/*   Updated: 2023/10/05 21:39:48 by mikuiper      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
+
+bool isPositiveNumber(const std::string &s)
+{
+    if (s.empty() || (s[0] == '0' && s.length() > 1))
+	{
+        return false; // Numbers starting with 0 are not positive
+    }
+    for (std::size_t i = 0; i < s.length(); ++i)
+	{
+        if (!std::isdigit(s[i]))
+		{
+            return false; // Non-digit characters found
+        }
+    }
+    return (true);
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+	{
+        std::cout << "Usage: " << argv[0] << " <positive_number_sequence>" << std::endl;
+        return 1;
+    }
+
+    std::string input(argv[1]);
+
+    if (isPositiveNumber(input))
+	{
+        std::cout << "The input sequence is correct and consists of positive numbers." << std::endl;
+    }
+	else
+	{
+        std::cout << "Invalid input. Please enter a valid positive number sequence." << std::endl;
+    }
+
+    return (0);
+}
 
 
 /*
