28267430185f6109942ff757c9f124185a7fd0a4
Mithras Kuipers <mithraskuipers@gmail.com>
2024-02-10 13:59:30 +0100

    Rework ex02. Currently still error w.r.t. duplicating values in after field

--- ex02/srcs/PmergeMe.cpp
+++ ex02/srcs/PmergeMe.cpp
@@ -1,216 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: mikuiper <mikuiper@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/09/16 17:09:26 by mikuiper      #+#    #+#                 */
+/*   Updated: 2024/01/22 14:21:35 by mikuiper      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "PmergeMe.hpp"
-#include <algorithm>
 
-PmergeMe::PmergeMe()
+PmergeMe::PmergeMe ()
 {
 }
 
-PmergeMe::~PmergeMe()
+PmergeMe::PmergeMe (const PmergeMe &other)
+{
+        this->operator=(other);
+}
+
+PmergeMe::~PmergeMe ()
 {
 }
 
-PmergeMe::PmergeMe(const PmergeMe &other)
+PmergeMe& PmergeMe::operator = (const PmergeMe& other)
 {
-    this->intVector = other.intVector;
-    this->intDeque = other.intDeque;
+        (void)other;
+        return(*this);
 }
-
-PmergeMe &PmergeMe::operator=(const PmergeMe &other)
-{
-    if (this != &other)
-    {
-        this->intVector = other.intVector;
-        this->intDeque = other.intDeque;
-    }
-    return (*this);
-}
-
-static bool printOutput = true;
-
-void PmergeMe::intVectorPrinter()
-{
-    for (std::vector<int>::iterator it = intVector.begin(); it != intVector.end(); it++)
-    {
-        std::cout << *it << " ";
-    }
-    std::cout << std::endl;
-}
-
-void PmergeMe::intDequePrinter()
-{
-    for (std::deque<int>::iterator it = intDeque.begin(); it != intDeque.end(); it++)
-    {
-        std::cout << *it << " ";
-    }
-    std::cout << std::endl;
-}
-
-void PmergeMe::setPrintOutput(bool enablePrint)
-{
-    printOutput = enablePrint;
-}
-
-void PmergeMe::readInput(char **argv)
-{
-    for (int left_idx = 1; argv[left_idx]; left_idx++)
-    {
-        this->intVector.push_back(std::atoi(argv[left_idx]));
-        this->intDeque.push_back(std::atoi(argv[left_idx]));
-    }
-}
-
-void PmergeMe::fordJohnsonSortWrapper()
-{
-    double duration;
-    clock_t start, stop;
-
-    std::cout << "Before: ";
-    this->intVectorPrinter();
-    start = clock();
-    this->fordJohnsonSort(this->intVector, 0, this->intVector.size() - 1);
-    stop = clock();
-    std::cout << "After:  ";
-    this->intVectorPrinter();
-    duration = ((stop - start) / (double)CLOCKS_PER_SEC) * 1000000;
-    std::cout << "Time to processExpression a range of " << this->intVector.size() << " elements with std::vector: ";
-    std::cout << duration << " us" << std::endl;
-
-    start = clock();
-    this->fordJohnsonSort(this->intDeque, 0, this->intDeque.size() - 1);
-    stop = clock();
-    duration = ((stop - start) / (double)CLOCKS_PER_SEC) * 1000000;
-    std::cout << "Time to processExpression a range of " << this->intDeque.size() << " elements with std::deque: ";
-    std::cout << duration << " us" << std::endl;
-}
-
-
-template <typename T>
-void PmergeMe::mergeSublists(T &lst, int lst_start_idx, int mid, int lst_end_idx)
-{
-    // Merge sublists in-place
-    int left_idx = lst_start_idx;
-    int right_idx = mid + 1;
-
-    // Temporary vector to store merged sublists
-    std::vector<int> temp(lst_end_idx - lst_start_idx + 1);
-    int temp_index = 0;
-
-    if (printOutput)
-    {
-        std::cout << "Merging sublists: <";
-
-        for (int i = lst_start_idx; i <= mid; ++i)
-        {
-            std::cout << lst[i];
-            if (i < mid)
-                std::cout << ", ";
-        }
-
-        std::cout << "> <";
-
-        for (int i = mid + 1; i <= lst_end_idx; ++i)
-        {
-            std::cout << lst[i];
-            if (i < lst_end_idx)
-                std::cout << ", ";
-        }
-    }
-
-    while (left_idx <= mid && right_idx <= lst_end_idx)
-    {
-        if (lst[left_idx] <= lst[right_idx])
-        {
-            temp[temp_index++] = lst[left_idx++];
-        }
-        else
-        {
-            temp[temp_index++] = lst[right_idx++];
-        }
-    }
-
-    while (left_idx <= mid)
-    {
-        temp[temp_index++] = lst[left_idx++];
-    }
-
-    while (right_idx <= lst_end_idx)
-    {
-        temp[temp_index++] = lst[right_idx++];
-    }
-
-    if (printOutput)
-    {
-        std::cout << ">" << std::endl;
-    }
-
-    // Print the intermediate state of the list after merging each pair of sublists
-    if (printOutput)
-    {
-        std::cout << "Merged state: ";
-		std::cout << "<";
-        for (int i = lst_start_idx; i <= lst_end_idx; ++i)
-        {
-            std::cout << lst[i];
-            if (i < lst_end_idx)
-                std::cout << ", ";
-        }
-		std::cout << ">";
-        std::cout << std::endl;
-    }
-
-    // Copy sorted elements back to the original list
-    for (int i = 0; i < temp_index; i++)
-    {
-        lst[lst_start_idx + i] = temp[i];
-    }
-
-    if (printOutput)
-    {
-        std::cout << "Sorted sublist: <";
-        for (int i = lst_start_idx; i <= lst_end_idx; ++i)
-        {
-            std::cout << lst[i];
-            if (i < lst_end_idx)
-                std::cout << ", ";
-        }
-        std::cout << ">" << std::endl;
-    }
-}
-
-
-
-
-
-template <typename T>
-void PmergeMe::fordJohnsonSort(T &lst, int lst_start_idx, int lst_end_idx)
-{
-    int lst_mid_idx;
-
-    if (lst_start_idx != lst_end_idx)
-    {
-        lst_mid_idx = lst_start_idx + ((lst_end_idx - lst_start_idx) / 2);
-
-        if (printOutput)
-        {
-            std::cout << "Splitting list: <";
-            for (int i = lst_start_idx; i <= lst_end_idx; ++i)
-            {
-                std::cout << lst[i];
-                if (i < lst_end_idx)
-                    std::cout << ", ";
-            }
-            std::cout << ">" << std::endl;
-        }
-
-        fordJohnsonSort(lst, lst_start_idx, lst_mid_idx);
-        fordJohnsonSort(lst, lst_mid_idx + 1, lst_end_idx);
-
-        mergeSublists(lst, lst_start_idx, lst_mid_idx, lst_end_idx);
-    }
-}
-
-
-
-// Explicit instantiation of template for specific types
-template void PmergeMe::fordJohnsonSort(std::vector<int> &lst, int lst_start_idx, int lst_end_idx);
-template void PmergeMe::fordJohnsonSort(std::deque<int> &lst, int lst_start_idx, int lst_end_idx);
--- ex02/srcs/main.cpp
+++ ex02/srcs/main.cpp
@@ -10,59 +10,139 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "PmergeMe.hpp"
-
 /*
+make re ; ./PmergeMe $(shuf -i 1-100 -n 10)
 make re ; ./PmergeMe $(shuf -i 10-100 -n 30) ;
 make re ; ./PmergeMe $(shuf -i 1-100000 -n 3000) ;
 */
 
-int isPositiveNumber(const std::string &s)
+#include "PmergeMe.hpp"
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <sys/time.h>
+
+bool isFlagSet = false;
+unsigned int flagValue;
+
+template <typename T, typename S, typename V>
+void processRange(int argc, T &container, S &cont, V &conta, char choice)
 {
-	if (s.empty())
-	{
-		return (0);
-	}
-	for (std::size_t left_idx = 0; left_idx < s.length(); left_idx++)
-	{
-		if (!std::isdigit(s[left_idx]))
-		{
-			return (0);
-		}
-	}
-	return (1);
+    struct timeval start, end;
+    long sec, micro;
+
+    gettimeofday(&start, NULL);
+    for (int i = 0; i < argc; i++)
+    {
+        if (container[i].first > container[i].second)
+            std::swap(container[i].first, container[i].second);
+    }
+
+    for (int i = 0; i < argc; i++)
+        conta.push_back(container[i].first);
+
+    for (int i = 0; i < argc; i++)
+        cont.push_back(container[i].second);
+
+    std::sort(conta.begin(), conta.end());
+
+    for (typename S::iterator it = cont.begin(); it != cont.end(); ++it)
+        conta.insert(std::lower_bound(conta.begin(), conta.end(), *it), *it);
+
+    if (isFlagSet)
+        conta.insert(std::lower_bound(conta.begin(), conta.end(), flagValue), flagValue);
+
+    std::cout << "\nAfter  : ";
+    for (typename V::iterator it = conta.begin(); it != conta.end(); ++it)
+        std::cout << *it << " ";
+
+    gettimeofday(&end, NULL);
+    sec = end.tv_sec - start.tv_sec;
+    micro = end.tv_usec - start.tv_usec;
+    long diff = (sec * 1000000) + micro;
+    if (choice == 'v')
+        std::cout << "\nTime to process a range of " << argc << " elements with std::vector : " << diff << " us" << "\n";
+    if (choice == 'd')
+        std::cout << "\nTime to process a range of " << argc << " elements with std::deque : " << diff << " us" << "\n";
 }
 
-int isInputValid(char **argv)
+void validateInput(char *arg1, char *arg2)
 {
-	for (int left_idx = 1; argv[left_idx]; left_idx++)
-	{
-		if (!isPositiveNumber(argv[left_idx]))
-		{
-			return (0);
-		}
-	}
-	return (1);
+    if (std::atoi(arg1) < 0 || std::atoi(arg2) < 0)
+    {
+        std::cerr << "Invalid Input: Number Less Than 0\n";
+        exit(0);
+    }
 }
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	if (argc < 2)
-	{
-		std::cout << "Usage: ./PmergeMe <positive_number_sequence>" << std::endl;
-		return 1;
-	}
+    if ((argc - 1) % 2 != 0)
+    {
+        isFlagSet = true;
+        flagValue = std::atoi(argv[argc - 1]);
+        if (flagValue < 0)
+            exit(1);
+        argc -= 1;
+    }
 
-	if (!isInputValid(argv))
-	{
-		std::cout << "Error: Invalid input" << std::endl;
-		return 1;
-	}
+    std::cout << "Before : ";
+    for (int i = 1; i < argc; i++)
+        std::cout << argv[i] << " ";
 
-	PmergeMe	mergeMe;
-	mergeMe.readInput(argv);
-	mergeMe.setPrintOutput(1);
-	mergeMe.fordJohnsonSortWrapper();
+    // VECTOR
+    std::vector<std::pair<unsigned int, unsigned int> > vec;
+    for (int i = 1; i < argc; i += 2)
+    {
+        if (isdigit(*argv[i]) && isdigit(*argv[i + 1]))
+        {
+            validateInput(argv[i], argv[i + 1]);
+            vec.push_back(std::make_pair(std::atoi(argv[i]), std::atoi(argv[i + 1])));
+        }
+        else
+        {
+            std::cerr << "Invalid input: non-numeric argument.\n";
+            return 1;
+        }
+    }
+    std::vector<unsigned int> vec_a, vec_b;
+    vec_a.reserve(vec.size());
+    vec_b.reserve(vec.size());
+    for (std::vector<std::pair<unsigned int, unsigned int> >::iterator it = vec.begin(); it != vec.end(); ++it)
+    {
+        vec_a.push_back(it->first);
+        vec_b.push_back(it->second);
+    }
 
-	return (0);
+    processRange(vec.size(), vec, vec_a, vec_b, 'v');
+
+    // DEQUE
+    std::deque<std::pair<unsigned int, unsigned int> > dec;
+    for (int i = 1; i < argc; i += 2)
+    {
+        if (isdigit(*argv[i]) && isdigit(*argv[i + 1]))
+        {
+            validateInput(argv[i], argv[i + 1]);
+            dec.push_back(std::make_pair(std::atoi(argv[i]), std::atoi(argv[i + 1])));
+        }
+        else
+        {
+            std::cerr << "Invalid input: non-numeric argument.\n";
+            return 1;
+        }
+    }
+
+    std::deque<unsigned int> dec_a, dec_b;
+    for (std::deque<std::pair<unsigned int, unsigned int> >::iterator it = dec.begin(); it != dec.end(); ++it)
+    {
+        dec_a.push_back(it->first);
+        dec_b.push_back(it->second);
+    }
+
+    processRange(dec.size(), dec, dec_a, dec_b, 'd');
+
+    return 0;
 }
